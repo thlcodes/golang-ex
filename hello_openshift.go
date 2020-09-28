476662a6b2d2459c98a4b3f566d4bcc09196a7b9
Thomas Liebeskind <thomas.liebeskind@gmail.com>
2020-09-28 23:27:19 +0200

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!"
+		response = "Hello OpenShift from " + os.Getenv("HOSTNAME")
 	}
 
 	fmt.Fprintln(w, response)
